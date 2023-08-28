0d5e750fdf5bf50e700b89158eb2d433447fc8f9
midorinom <107839253+midorinom@users.noreply.github.com>
2023-08-29 01:37:48 +0800

    fixed lint errors

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# import auth
+# import events
+
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-import auth, events
-
 if __name__ == "__main__":
+
     app.run(port=8000, debug=True)
