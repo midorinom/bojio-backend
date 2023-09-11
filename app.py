86c4362168dd79e0a030a8fecd35af0ecef3c0d8
Hao Fang <haofang97@gmail.com>
2023-09-11 16:12:29 +0800

    testing database

--- app.py
+++ app.py
@@ -4,15 +4,15 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from controllers import demo_controller
+from models import user_model,demo_model
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:j1a2c3k4@localhost/bojio_db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:j1a2c3k4@localhost/bojio_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Password1234@bojio-db.c3tfalfhp9o3.ap-southeast-1.rds.amazonaws.com/bojio-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
-
-from controllers import demo_controller
-from models import user_model,demo_model
 
 if __name__ == "__main__":
     # Creates DB tables based on models created in models folder, only if they don't exists
