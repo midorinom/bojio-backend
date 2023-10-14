714a543c9a2942e8adf7bd4ffd6f094cb9938ea1
Tan Sheng Fong <tanshengfong@icloud.com>
2023-10-14 15:00:26 +0800

    Removed config.py from remote and added to gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-class ProductionConfig(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:password1234@bojio-db.c3tfalfhp9o3.ap-southeast-1.rds.amazonaws.com/bojio-db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:''@localhost/bojio_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
