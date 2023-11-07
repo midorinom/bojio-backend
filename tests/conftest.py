5b72db8fa9fd95b9528dcc792434c6984f5ee610
Tan Sheng Fong <tanshengfong@icloud.com>
2023-11-07 15:20:39 +0800

    test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,6 @@ from flask_cors import CORS
 @pytest.fixture()
 def app():
     app = create_app()
-    
-    # app.config['SESSION_SQLALCHEMY'] = db
-    # session = Session(app)
-    # CORS(app, supports_credentials=True)
-
-    # # Creates DB tables based on models created in models folder, only if they don't exists
-    # with app.app_context():
-    #     session.app.session_interface.db.create_all()
-
     yield app
 
 @pytest.fixture()
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,22 @@
-def test_event(client):
-    response = client.get('/demo/test')
+import json
+
+
+def test_event_controller(client):
+    response = client.get('/event/test')
+    assert response.status_code == 200
+
+def test_retrieve_all_events(client):
+    response = client.get('/event/all-events')
+    assert response.status_code == 200
+
+def test_register(client):
+    headers = {'Content-Type': 'application/json'}
+    data = json.dumps({"username": "wala3", "password": "162534", "email": "abcd@outlook.com"})
+    response = client.post('/register', data=data, headers=headers)
+    assert response.status_code == 200
+
+def test_login(client):
+    headers = {'Content-Type': 'application/json'}
+    data = json.dumps({"username": "wala3", "password": "162534"})
+    response = client.post('/login', data=data, headers=headers)
     assert response.status_code == 200
